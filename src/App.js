ae4f958f30e2537ee66ee5c515f4b4d54d59f3b8
amal <amal.sarkar@sentienz.com>
2021-10-26 19:49:07 +0530

    initail work

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Amal
         </a>
       </header>
     </div>
